7faa18bd0265407c312516129cb1104e660134d5
ClarkTHoward <clark.t.howard@gmail.com>
2023-09-21 15:14:08 -0600

    finished the print and sort

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,6 +1,8 @@
-import java.awt.Desktop;  
-import java.io.*;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * @author Clark Howard
@@ -11,20 +13,36 @@ import java.util.ArrayList;
 public class ScoreTrakker {
 	private ArrayList<Student> students;
 	public ScoreTrakker() {
-
+		students = new ArrayList<Student>();
 	}
 	public void loadDataFile(String fileName)
 	{
-		File file = new File(fileName);
-
+		try {
+		      File file = new File(fileName);
+		      Scanner reader = new Scanner(file);
+		      while (reader.hasNextLine()) {
+		        String name = reader.nextLine();
+		        int score = reader.nextInt();
+		        reader.nextLine();
+		    	students.add(new Student(name,score));
+		      }
+		      reader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
 	}
 	public void printInOrder()
 	{
+		Collections.sort(students);
 		for(Student student: students)
+		{
+			System.out.println(student);
+		}
 	}
 	public void processFiles()
 	{
-		printInOrder();
+		loadDataFile("scores.txt");
 		printInOrder();
 	}
 	public static void main(String[] args) {
