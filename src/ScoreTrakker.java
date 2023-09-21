9628a8375b1a259f714e24f737b81933f44cc9da
rgrundmeierCSM <rgrundmeier@mines.edu>
2023-09-21 15:13:11 -0600

    Implemented working version of loadDataFile

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,6 +1,7 @@
 import java.awt.Desktop;  
 import java.io.*;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.Scanner; 
 
 /**
  * @author Clark Howard
@@ -11,25 +12,48 @@ import java.util.ArrayList;
 public class ScoreTrakker {
 	private ArrayList<Student> students;
 	public ScoreTrakker() {
+		students = new ArrayList<Student>();
+		
 
 	}
-	public void loadDataFile(String fileName)
+	public void loadDataFile(String fileName) throws FileNotFoundException
 	{
-		File file = new File(fileName);
+		FileReader reader = new FileReader(fileName);
+		Scanner in  = new Scanner(reader);
+		//While there are more lines to read
+		while(in.hasNextLine()) {
+		//read the first two values and store them as a  string
+		String tempString = in.next();
+		tempString = tempString + " " + in.next();
+		//read in the integer value and create a Student object
+		int tempInt = in.nextInt();
+		students.add(new Student(tempString, tempInt));
+		
+		
+	
+		}
+		
+		in.close();
 
 	}
 	public void printInOrder()
 	{
-		for(Student student: students)
-	}
+		for(Student student: students) {
+		}
+		}
+	
 	public void processFiles()
 	{
 		printInOrder();
 		printInOrder();
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException{
 		ScoreTrakker s = new ScoreTrakker();
-		s.processFiles();
-	}
-
+		s.loadDataFile("scores.txt");
+		System.out.println(s.students.get(1));
+		
+		
+	}	
+	
 }
+
