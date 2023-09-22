ba48a9d58cc01c77872f76667505e16894904e7e
rgrundmeierCSM <rgrundmeier@mines.edu>
2023-09-22 04:07:24 -0600

    Updated comments and cleaned up code

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -20,6 +20,11 @@ public class ScoreTrakker {
 		students = new ArrayList<Student>();
 
 	}
+	/**
+	 * 
+	 * @param fileName
+	 * @throws FileNotFoundException, error to be handled by processFiles()
+	 */
 	public void loadDataFile(String fileName) throws FileNotFoundException
 	{
 		FileReader reader = new FileReader(fileName);
@@ -43,49 +48,46 @@ public class ScoreTrakker {
 			catch (NumberFormatException e){
 				System.out.println("Incorrect format for " + tempString + " not a valid score: " + tempString2);
 				System.out.println("");
-				
-			
-
 			}
-			
-
-
-			
-
-
-
 		}
-
 		in.close();
-
 	}
+	
+	
+	/**
+	 * Sorts the items in the ArrayList and then prints them out with appropriate labels. 
+	 */
 	public void printInOrder()
 	{
 		//Put the Students in the correct order
 		Collections.sort(students);
 		//Print out the header statement
 		System.out.println("Student Score List");
-		
-		for(Student student: students)
-		{
+		//Iterate through the ArrayList and print out the objects
+		for(Student student: students)	{
 			System.out.println(student);
 		}
 		//Add a blank line after the list
 		System.out.println("");
 	}
+	
+	
+	/**
+	 * Loads and prints the information from the array of file names. 
+	 * @throws FileNotFoundException - this will go away, exception handling will be added 
+	 * to this function as a part of the requirement. 
+	 */
 	public void processFiles() throws FileNotFoundException	{
 		for (int i = 0; i < 3; i++) {
 			loadDataFile(files[i]);
 			printInOrder();
 		}
-
 	}
+	
+	
 	public static void main(String[] args) throws FileNotFoundException{
 		ScoreTrakker s = new ScoreTrakker();
 		s.processFiles();
-
-
 	}	
-
 }
 
