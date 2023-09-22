592ad2b923e60ac2a5dd7d9aa0670153a8bd65dc
rgrundmeierCSM <rgrundmeier@mines.edu>
2023-09-22 04:01:18 -0600

    Added badscore.txt, updated method loadDataFile to correctly handle exceptions regarding non-integer score inputs from the file.

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.io.File; 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 /**
@@ -11,50 +11,81 @@ import java.util.Scanner;
  * 
  */
 public class ScoreTrakker {
+
 	private ArrayList<Student> students;
+	private String [] files = {"scores.txt", "badscore.txt", "nofile.txt"};
+
+
 	public ScoreTrakker() {
 		students = new ArrayList<Student>();
+
 	}
 	public void loadDataFile(String fileName) throws FileNotFoundException
 	{
 		FileReader reader = new FileReader(fileName);
 		Scanner in  = new Scanner(reader);
+		students = new ArrayList<Student>();
+
 		//While there are more lines to read
 		while(in.hasNextLine()) {
-		//read the first two values and store them as a  string
-		String tempString = in.next();
-		tempString = tempString + " " + in.next();
-		//read in the integer value and create a Student object
-		int tempInt = in.nextInt();
-		students.add(new Student(tempString, tempInt));
-		
-		
-	
+			int tempInt = 0;
+			//read the first two values and store them as a  string
+			String tempString = in.next();
+			tempString = tempString + " " + in.next();
+			//read in the integer value and create a Student object
+			String tempString2 = in.next();
+			// Error handling to check that the score is formatted as an integer. 
+			try {
+				tempInt = Integer.parseInt(tempString2);
+				students.add(new Student(tempString, tempInt));
+			}
+
+			catch (NumberFormatException e){
+				System.out.println("Incorrect format for " + tempString + " not a valid score: " + tempString2);
+				System.out.println("");
+				
+			
+
+			}
+			
+
+
+			
+
+
+
 		}
-		
+
 		in.close();
 
 	}
 	public void printInOrder()
 	{
+		//Put the Students in the correct order
 		Collections.sort(students);
+		//Print out the header statement
+		System.out.println("Student Score List");
+		
 		for(Student student: students)
 		{
 			System.out.println(student);
 		}
+		//Add a blank line after the list
+		System.out.println("");
 	}
-	public void processFiles()
-	{
-		loadDataFile("scores.txt");
-		printInOrder();
+	public void processFiles() throws FileNotFoundException	{
+		for (int i = 0; i < 3; i++) {
+			loadDataFile(files[i]);
+			printInOrder();
+		}
+
 	}
 	public static void main(String[] args) throws FileNotFoundException{
 		ScoreTrakker s = new ScoreTrakker();
-		s.loadDataFile("scores.txt");
-		System.out.println(s.students.get(1));
-		
-		
+		s.processFiles();
+
+
 	}	
-	
+
 }
 
