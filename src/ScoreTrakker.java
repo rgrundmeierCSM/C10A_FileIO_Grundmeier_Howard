e3ea3e5bd2e6cd1402c595bb329632937bc4864d
rgrundmeierCSM <rgrundmeier@mines.edu>
2023-09-23 17:21:45 -0600

    Reverted to previous version

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
@@ -15,7 +16,6 @@ public class ScoreTrakker {
 	private ArrayList<Student> students;
 	private String [] files = {"scores.txt", "badscore.txt", "nofile.txt"};
 
-
 	public ScoreTrakker() {
 		students = new ArrayList<Student>();
 
@@ -25,34 +25,31 @@ public class ScoreTrakker {
 	 * @param fileName
 	 * @throws FileNotFoundException, error to be handled by processFiles()
 	 */
-	public void loadDataFile(String fileName) throws FileNotFoundException
+	public void loadDataFile(String fileName)
 	{
-		FileReader reader = new FileReader(fileName);
-		Scanner in  = new Scanner(reader);
-		students = new ArrayList<Student>();
-
-		//While there are more lines to read
-		while(in.hasNextLine()) {
-			int tempInt = 0;
-			//read the first two values and store them as a  string
-			String tempString = in.next();
-			tempString = tempString + " " + in.next();
-			//read in the integer value and create a Student object
-			String tempString2 = in.next();
-			// Error handling to check that the score is formatted as an integer. 
-			try {
-				tempInt = Integer.parseInt(tempString2);
-				students.add(new Student(tempString, tempInt));
+		FileReader reader;
+		try {
+			reader = new FileReader(fileName);
+			Scanner in  = new Scanner(reader);
+			//While there are more lines to read
+			while(in.hasNextLine()) {
+				//Reads the first number in as a string
+				String Name = in.nextLine();
+				System.out.println(Name);
+				String scoreString = in.nextLine();
+				int score = Integer.parseInt(scoreString);
+				students.add(new Student(Name, score));
 			}
-
-			catch (NumberFormatException e){
-				System.out.println("Incorrect format for " + tempString + " not a valid score: " + tempString2);
-				System.out.println("");
-			}
+			in.close();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		in.close();
 	}
-	
 	
 	/**
 	 * Sorts the items in the ArrayList and then prints them out with appropriate labels. 
@@ -71,21 +68,19 @@ public class ScoreTrakker {
 		System.out.println("");
 	}
 	
-	
 	/**
 	 * Loads and prints the information from the array of file names. 
 	 * @throws FileNotFoundException - this will go away, exception handling will be added 
 	 * to this function as a part of the requirement. 
 	 */
-	public void processFiles() throws FileNotFoundException	{
+	public void processFiles() {
 		for (int i = 0; i < 3; i++) {
 			loadDataFile(files[i]);
 			printInOrder();
 		}
 	}
 	
-	
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args) {
 		ScoreTrakker s = new ScoreTrakker();
 		s.processFiles();
 	}	
